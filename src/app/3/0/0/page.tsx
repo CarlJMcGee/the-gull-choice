3e3d26bef7b0eac6af7ea974e76a9315892c78e6
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-11-09 08:58:02 -0600

    3/0/0/0

---
+++ src/app/3/0/0/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Prompt from "../../../../components/Prompt";
+
+interface IPageProps {}
+
+export default function Page(props: IPageProps) {
+  return (
+    <>
+      <Prompt
+        linkHead="/3/0/0/"
+        prompt={[
+          "The warm milk splatters across the gulls toned legs,",
+          "she screeches, hopping 10ft in place, as the druid reaches into his cloak pocket.",
+        ]}
+        choices={[
+          {
+            title: "Roll Inititive.",
+            body: "Determine the order of combat.",
+          },
+          {
+            title: "Ninja Vanish.",
+            body: "Blend into your envionment and disapear from sight.",
+          },
+          {
+            title: "Be on your Guard.",
+            body: "Take a defensive postion low to the ground, hands high in the air.",
+          },
+          {
+            title: "Be Insertive",
+            body: "Grab for whatever is in the the young man's cloak.",
+          },
+        ]}
+      />
+    </>
+  );
+}
