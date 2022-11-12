70de91d4108184d491307772908eae277fd37d4b
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-11-11 19:59:32 -0600

    did stuff, forgot to commit earlier.

---
+++ src/app/3/1/2/3/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import EndCard from "../../../../../components/EndCard";
+
+export default function Page() {
+  return (
+    <>
+      <EndCard
+        prompt={[
+          "What?",
+          `I don't understand what you're asking. Who are you and how did you get my number? 
+        Please do not call my again or I'll have to get the police involved. Goodbye.`,
+        ]}
+      />
+    </>
+  );
+}
