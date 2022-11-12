47a423d17d199c3c51539980dc703da456d0d07b
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-11-11 21:03:39 -0600

    added head file to appDir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,16 +7,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
-      <head>
-        <title>The Gull Choice</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="description"
-          content="The choice has always been yours, my friend..."
-        />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-      </head>
+    <html lang="en">
+      <head />
       <body>
         <div className="m-5 flex flex-row justify-center">
           <Image src={gull} alt="the gull and the druid" width={500} />
