1facaf71dc583e94ae55c99ff77ca997bb3bf246
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-11-15 17:54:46 -0600

    3/2 complete

---
+++ src/app/3/2/page.tsx
@@ -0,0 +1,17 @@
+import EndCard from "../../components/EndCard";
+
+export default function () {
+  return (
+    <>
+      <EndCard
+        prompt={[
+          `The milk seeps into the poor druid's eyes,`,
+          `"Ah, my eyes!" he screams. You take the oppertunity to grab the gull and run. 
+        After some time, you finally stop to rest. The fragile gull looks up at you and says, 
+        "Thank you stranger, that man stole me from my family years ago. Will you help me find them?" 
+        You smile at the bird with the glint of adventure in your eye, answering, "Where do we start?"`,
+        ]}
+      />
+    </>
+  );
+}
