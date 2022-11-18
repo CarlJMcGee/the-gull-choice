eff1aa23e845a249078c2341ea01524a589a9832
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-11-17 23:46:37 -0600

    added homeand back buttons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import Image from "next/image";
 import gull from "./assets/gull-milk(cropped).jpg";
+import Nav from "./components/Nav";
 export default function RootLayout({
   children,
 }: {
@@ -10,6 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body>
+        <Nav />
         <div className="m-5 flex flex-row justify-center">
           <Image src={gull} alt="the gull and the druid" width={500} />
         </div>
